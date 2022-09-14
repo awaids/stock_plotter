670b087266d9747e099a15581d42203598250817
Awaid Shaheen <awaidsha@amd.com>
2022-09-14 16:58:48 +0200

    Adding tests for the CatTrader

--- Trader/CatTrader.py
+++ Trader/CatTrader.py
@@ -1,12 +1,124 @@
 import pygame
 from os.path import dirname, join
-from Helper import DrawBase
+from stock_plotter.Helper import DrawBase
+from stock_plotter.Helper import Action
+
 
 class CatTrader(DrawBase):
-    def __init__(self) -> None:
+    def __init__(self, starting_capital:float = 1000.0) -> None:
+        #TODO: figure out how to make the image here universal! -> needs pygame init
         image = pygame.image.load(join(dirname(__file__), "TraderCat.png")).convert()
         self.image = pygame.transform.scale(image, (50, 50))
+        # Used by reset to properly setup variables
+        self.initial_starting_capital = starting_capital
+        # Use the reset as intializing point for all the member variables
+        self.reset(self.initial_starting_capital)
 
     def draw(self, parent_surface: pygame.Surface) -> pygame.Surface:
         parent_surface.blit(self.image, (0,0))
         return parent_surface
+
+    def reset(self, starting_capital:float = 1000.0) -> None:
+      # Once this is false, this means that the trader is dead. We must not process this,
+        # however we can still draw the death statistics!
+        self._isDead = False
+        self._inTrade = False
+
+        # Trader state values
+        assert(starting_capital > 0), "Starting money for CatTrader must be greater than 0"
+        self._capital = starting_capital    # Current capital
+        self._unrealized_gains = 0.0    # Potential gain/loss for if in trade
+        self._trades = 0    # No. of trader done uptil now
+        self._cum_losses = 0.0  # Cummulative loss done on all trades
+        self._cum_profits = 0.0 # Cummulative Profit done on all trades
+        self._holding = 0.0 # No. of coins being held
+
+        self._buy_price = 0.0
+
+    # Useful properties
+    @property
+    def isDead(self) -> bool:
+        return self._isDead
+
+    @property
+    def current_capital(self) -> float:
+        return self._capital
+    
+    @property
+    def assets_holding(self) -> float:
+        return self._holding
+    
+    @property
+    def cum_profit(self) -> float:
+        return self._cum_profits
+
+    @property
+    def cum_losses(self) -> float:
+        return self._cum_losses
+
+    @property
+    def trades_done(self) -> int:
+        # Trade will only be done one buy and sell happens
+        return self._trades
+
+    @property
+    def unrealized_gains(self) -> float:
+        return self._unrealized_gains
+    
+    def process(self, action:Action, close:float) -> float:
+        # This will change the internal variables values
+        assert(close != 0.0), "Close values cannot be 0"
+        reward = self._compute_reward(action, close)
+        self._update(action, close)
+        return reward
+
+    def _update(self, action:Action, close:float) -> None:
+        if action == Action.BUY:
+            # Only buy when in not trade
+            if not self._inTrade:
+                self._holding = self._capital / close
+                self._capital -= self._holding * close
+                self._buy_price = close
+                self._inTrade = True
+        elif action == Action.HOLD:
+            # Only update the unrealized gains if in trade
+            if self._inTrade:
+                self._unrealized_gains =  (close - self._buy_price) * self._holding
+        elif action == Action.SELL:
+            # Only sell when already in trade
+            if self._inTrade:
+                # Determine to add to profit or loss
+                pnl = (close - self._buy_price) * self._holding
+                if pnl > 0.0:
+                    self._cum_profits += pnl
+                else:
+                    self._cum_losses +=  abs(pnl)
+
+                # Reset the other values
+                self._capital += close * self._holding
+                self._holding = 0.0
+                self._buy_price = 0.0
+                self._unrealized_gains = 0.0
+                self._inTrade = False
+
+                #Increment trade
+                self._trades += 1
+
+    # Here are the main reward functions
+    def _compute_reward(self, action:Action, close:float) -> float:
+        reward = None
+        if action == Action.BUY:
+            reward = 1.0 if not self._inTrade else -1.0
+        elif action == Action.HOLD:
+            if self._inTrade:
+                reward = 0.5 if close > self._buy_price else -0.5
+            else:
+                reward = -0.1
+        elif action == Action.SELL:
+            if self._inTrade:
+                change_percentage = (close - self._buy_price)/self._buy_price
+                reward = 1 + change_percentage if change_percentage > 0.0 else - 2 - change_percentage
+            else:
+                reward = -1.0
+        assert(reward), "NONE reward computed!"
+        return reward
---
+++ Trader/test_cattrader.py
@@ -0,0 +1,115 @@
+import pytest
+from stock_plotter.Helper.Bases import Action
+from stock_plotter.Trader import CatTrader
+from stock_plotter.Helper.Functions import setup_disply
+
+class TestCatTrader:
+    setup_disply()
+    Trader = CatTrader(starting_capital=100)
+    
+    def get_internals(self) -> list:
+        # Returns the values of the CatTrader variables
+        trader = self.Trader
+        return [
+            trader.current_capital,
+            trader.assets_holding,
+            trader.unrealized_gains,
+            trader.cum_profit,
+            trader.cum_losses,
+            trader.trades_done
+        ]
+
+    def test_process_zero_close_assert(self):
+        with pytest.raises(AssertionError):
+            self.Trader.process(action=Action.BUY, close=0)
+
+    def test_process_default(self):
+        self.Trader.reset(starting_capital=100)
+        assert(self.get_internals() == [100.0, 0.0, 0.0, 0.0, 0.0, 0])
+
+    def test_process_trade_buy_sell_profit(self):
+        self.Trader.reset(starting_capital=100)
+
+        # Buy
+        self.Trader.process(action=Action.BUY, close=10.0)
+        assert(self.get_internals() == [0.0, 10.0, 0.0, 0.0, 0.0, 0])
+
+        # Sell with profit
+        self.Trader.process(action=Action.SELL, close=20.0)
+        print(self.get_internals())
+        assert(self.get_internals() == [200.0, 0.0, 0.0, 100.0, 0.0, 1])
+
+    def test_process_trade_buy_sell_loss(self):
+        self.Trader.reset(starting_capital=100)
+        # Buy
+        self.Trader.process(action=Action.BUY, close=10.0)
+        assert(self.get_internals() == [0.0, 10.0, 0.0, 0.0, 0.0, 0])
+
+        # Sell with loss
+        self.Trader.process(action=Action.SELL, close=5.0)
+        assert(self.get_internals() == [50.0, 0.0, 0.0, 0.0, 50.0, 1])
+
+    def test_process_hold_cases(self):
+        self.Trader.reset(starting_capital=100)
+
+        # Hold with nothing
+        self.Trader.process(action=Action.HOLD, close=20.0)
+        assert(self.get_internals() == [100.0, 0.0, 0.0, 0.0, 0.0, 0])
+
+        # Buy
+        self.Trader.process(action=Action.BUY, close=10.0)
+
+        # Hold with unrealized gains
+        self.Trader.process(action=Action.HOLD, close=20.0)
+        assert(self.get_internals() == [0.0, 10.0, 100.0, 0.0, 0.0, 0])
+
+        # Hold with unrealized losses
+        self.Trader.process(action=Action.HOLD, close=5.0)
+        assert(self.get_internals() == [0.0, 10.0, -50.0, 0.0, 0.0, 0])
+
+        # Sell with profit
+        self.Trader.process(action=Action.SELL, close=20.0)
+
+        # Hold with some profit
+        self.Trader.process(action=Action.HOLD, close=25.0)
+        assert(self.get_internals() == [200.0, 0.0, 0.0, 100.0, 0.0, 1])
+    
+    def test_process_multiple_trades(self):
+        self.Trader.reset(starting_capital=100)
+        # Buy & sell
+        self.Trader.process(action=Action.BUY, close=10.0)
+        self.Trader.process(action=Action.SELL, close=20.0)
+        self.Trader.process(action=Action.BUY, close=20.0)
+        self.Trader.process(action=Action.SELL, close=10.0)
+        assert(self.get_internals() == [100.0, 0.0, 0.0, 100.0, 100.0, 2])
+
+
+    def test_rewards(self):
+        # General test for rewards -> Not fixing it to explicit values
+        self.Trader.reset(starting_capital=100)
+
+        # Test actions when not in trade
+        assert(self.Trader.process(action=Action.HOLD, close=10.0) < 0)
+        assert(self.Trader.process(action=Action.SELL, close=10.0) < 0)
+
+        # Test actions in trade
+        assert(self.Trader.process(action=Action.BUY, close=10.0) > 0)
+        # Buying while in trade NO NO
+        assert(self.Trader.process(action=Action.BUY, close=10.0) < 0)
+        # Test holds
+        assert(self.Trader.process(action=Action.HOLD, close=10.0) < 0)
+        assert(self.Trader.process(action=Action.HOLD, close=5.0) < 0)
+        assert(self.Trader.process(action=Action.HOLD, close=50.0) > 0)
+
+        #Test selling 
+        self.Trader.reset(starting_capital=100)
+        self.Trader.process(action=Action.BUY, close=10.0)
+        assert(self.Trader.process(action=Action.SELL, close=10.0) < 0)
+
+        self.Trader.reset(starting_capital=100)
+        self.Trader.process(action=Action.BUY, close=10.0)
+        assert(self.Trader.process(action=Action.SELL, close=5.0) < 0)
+
+        self.Trader.reset(starting_capital=100)
+        self.Trader.process(action=Action.BUY, close=10.0)
+        assert(self.Trader.process(action=Action.SELL, close=50.0) > 0)
